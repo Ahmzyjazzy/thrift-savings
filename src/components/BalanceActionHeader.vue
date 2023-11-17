de0e9182d40645b4c8677698d5df401049bfd499
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2023-11-17 05:44:31 +0100

    feat: implement shimmer loading for networj request

--- src/components/BalanceActionHeader.vue
+++ src/components/BalanceActionHeader.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { watch, onMounted } from 'vue'
-
+import LvSkeleton from 'lightvue/skeleton';
 import { balanceLoading, fetchActiveBalance, thrifBalance, formatAmount } from '../vueutils/useThrift'
 
 onMounted(() => {
@@ -11,7 +11,55 @@ onMounted(() => {
 
 
 <template>
-    <div class="flex justify-between items-start">
+    <div v-if="balanceLoading" class="flex justify-between items-start">
+        <div>
+            <div class="mb-5">
+                <h5 class="font-medium text-xl text-orange-typography mb-3 font-museo">
+                    <lv-skeleton :width="150" :height="28" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                </h5>
+                <!-- FDB5A7 -->
+                <h1 class="flex flex-row items-center relative text-white">
+                    <span class="text-md lg:text-xl mr-3 font-semibold leading-1">
+                        <lv-skeleton :width="36" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                    <span class="text-3xl lg:text-6xl font-semibold font-recoleta">
+                        <lv-skeleton :width="200" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                    <span class="text-md lg:text-xl font-semibold ml-2">
+                        <lv-skeleton :width="36" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                </h1>
+            </div>
+            <div>
+                <h5 class="font-medium text-xl text-orange-typography mb-3 font-museo">
+                    <lv-skeleton :width="150" :height="28" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                </h5>
+                <!-- FDB5A7 -->
+                <h1 class="flex flex-row items-center relative text-white">
+                    <span class="text-md mr-3 font-semibold leading-1">
+                        <lv-skeleton :width="36" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                    <span class="text-3xl font-semibold font-recoleta">
+                        <lv-skeleton :width="150" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                    <span class="text-md font-semibold ml-2">
+                        <lv-skeleton :width="36" :height="36" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
+                    </span>
+                </h1>
+            </div>
+        </div>
+        <router-link :to="{ 'name': 'create-thrift' }" title="Create Thrif Plan">
+            <div 
+                class=" border-8 bg-gray-800 border-[#FAA38E] w-[80px] h-[80px] lg:w-[100px] lg:h-[100px] rounded-full grid place-items-center">
+                <!-- add profile pix -->
+                    <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                    </svg>
+            </div>
+        </router-link>
+    </div>
+
+    <div v-else class="flex justify-between items-start">
         <div>
             <div class="mb-5">
                 <h5 class="font-medium text-xl text-orange-typography mb-3 font-museo">Total Saved:</h5>
@@ -44,7 +92,7 @@ onMounted(() => {
     </div>
 
     <!-- button component -->
-    <section class="relative my-10">
+    <section class="relative my-10" >
         <div class="grid grid-cols-3 rounded-md shadow-sm w-full" role="group">
             <button type="button" class="px-4 py-2 text-sm font-medium text-gray-50 bg-gray-700 border border-gray-800 rounded-l-lg hover:bg-gray-700 hover:text-gray-50">
                 Profile
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { watch, onMounted } from 'vue'
-
+import LvSkeleton from 'lightvue/skeleton';
 import HomeLayout from '../layouts/HomeLayout.vue';
 import ThriftCard from '../components/ThriftCard.vue';
 import TabSwitch from '../components/TabSwitch.vue';
 import BalanceActionHeader from '../components/BalanceActionHeader.vue';
 
-import { loading, fetchThriftSummary, allThrifts, activeFilter } from '../vueutils/useThrift'
+import { loading, fetchThriftSummary, allThrifts, activeFilter, balanceLoading } from '../vueutils/useThrift'
 
 onMounted(() => {
     fetchThriftSummary()
@@ -44,8 +44,8 @@ watch(activeFilter, () => {
 
             <div class="tabs py-10">
                 <div class="tabs-content">
-                    <div v-if="loading" class="w-full bg-white border border-gray-50 rounded-2xl shadow min-h-[100px] px-5 py-5 mb-5 grid place-items-center">
-                        Loading, Please wait...
+                    <div v-if="loading" class="w-full bg-white border border-gray-50 rounded-2xl shadow min-h-[100px] px-5 py-5 mb-5 grid grid-cols-1 place-items-center">
+                        <lv-skeleton :width="150" :height="20" primaryColor="#f6c6bc" secondaryColor="#FEF0EE" />
                     </div>
                     <template 
                         v-else
